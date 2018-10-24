0b1deb55c4610f2bcaea5846aab85477b1731222
Aaron Gullickson <aarong@uoregon.edu>
2018-10-24 13:00:50 -0700

    initial commit

---
+++ analysis.R
@@ -0,0 +1,9 @@
+#Keep all the code for your analyses here. This may include supplementary or exploratory analyses 
+#that never make it into the final project. If you prefer, you can use the analysis.Rmd file instead
+#to record your analysis in a lab notebook style. 
+
+#load necessary libraries and custom functions
+source("check_packages.R")
+source("useful_functions.R")
+
+#load the data from your output directory
